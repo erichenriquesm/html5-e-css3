d8f819739cb84842e6b57df846acd8df1f701cc2
erichenriquesm <erichenriquedeveloper@gmail.com>
2022-08-07 01:57:08 -0300

    teste1

--- práticas/prática021/script.js
+++
@@ -1,9 +0,0 @@
-function checar(){
-    let nome = document.getElementById('nome')
-    let resultado = document.getElementById('res')
-    if(nome == "Eric"){
-        resultado.innerText = "Olá, lindo"
-    }else{
-        resultado.innerHTML = "Olá, ", nome 
-    }
-}
