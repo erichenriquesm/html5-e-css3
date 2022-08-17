c0dd9ecfd916dbaf703360a61e499de401b856dc
erichenriquesm <erichenriquedeveloper@gmail.com>
2022-08-17 15:37:40 -0300

    ajustes

--- DOM/interatividade/bd.js
+++ DOM/interatividade/bd.js
@@ -26,8 +26,11 @@ function per1(){
     let perfil1 = criarPerfil('per1/user.png', 'Eric', 'Olá, sou criador deste site em que mostro um pouco de HTML5, CSS3 E JavaScript utilizando domínio de objetos para deixar todas as informações dinâmicas. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta fugit, sunt natus eveniet voluptas officiis quod. Similique, sed amet expedita quibusdam nisi quasi ab unde enim neque! Cupiditate, expedita obcaecati?', 'https://marquesfernandes.com/wp-content/uploads/2020/01/1555172-768x432.jpg')
 
 
-    let labels = document.querySelectorAll('label')
-    labels.forEach((label) => label.style.border = '1px solid yellow');
+    let fields = document.querySelectorAll('fieldset')
+    fields.forEach((field) =>{ 
+    field.style.color = 'yellow'
+    field.style.border = '1px solid yellow '
+});
 
     let usuario = document.querySelector('#nome');
     usuario.style.color = 'yellow';
@@ -42,8 +45,11 @@ function per1(){
 function per2(){
     let perfil2 = criarPerfil('per2/user.png', 'Leonardo', 'Olá, sou Leonardo, gosto de Star Wars e hoje estou dentro do projeto do meu amigo, espero que gostem. ectetur adipisicing elit. Soluta fugit, sunt natus eveniet voluptas officiis quod. Similique, sed amet expedita quibusdam nisi quasi ab unde enim neque! Cupiditate,ectetur adipisicing elit. Soluta fugit, sunt natus eveniet voluptas officiis quod. Similique, sed amet expedita quibusdam nisi quasi ab unde enim neque! Cupiditate,', 'https://mcdn.wallpapersafari.com/medium/38/65/Fcx7LA.jpg');
 
-    let labels = document.querySelectorAll('label')
-    labels.forEach((label) => label.style.border = '1px solid purple');
+    let fields = document.querySelectorAll('fieldset')
+    fields.forEach((field) =>{ 
+    field.style.color = 'purple'
+    field.style.border = '1px solid purple'
+});
 
     let usuario = document.querySelector('#nome');
     usuario.style.color = 'purple';
@@ -60,8 +66,11 @@ function per3(){
     let perfil3 = criarPerfil('per3/user (2).jpeg', 'Yasmim', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta fugit, sunt natus eveniet voluptas officiis quod. Similique, sed amet expedita quibusdam nisi quasi ab unde enim neque! Cupiditate, expedita obcaecati?', 'per3/back.png')
 
 
-    let labels = document.querySelectorAll('label')
-    labels.forEach((label) => label.style.border = '1px solid #E515A7');
+    let fields = document.querySelectorAll('fieldset')
+    fields.forEach((field) =>{ 
+    field.style.color = '#E515A7'
+    field.style.border = '1px solid #E515A7 '
+});
 
     let usuario = document.querySelector('#nome');
     usuario.style.color = '#E515A7';
